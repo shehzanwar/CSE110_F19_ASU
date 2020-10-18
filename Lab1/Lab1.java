a97041018adbd637d75fad21aa79309a74a8e24d
Shehzad Anwar <cougar59912@gmail.com>
2020-10-17 18:16:40 -0700

    Lab 1

---
+++ Lab1/Lab1.java
@@ -0,0 +1,16 @@
+//Name: Shehzad Anwar
+//Lab Letter: H
+//Title: Lab1.java
+//Description: Prints out my first program and my name.
+//Date: 08/30/2019
+
+public class Lab1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, my name is Shehzad Anwar.");
+		System.out.println("This is my first program!");
+
+	}
+
+}
