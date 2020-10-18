923689a4537c08cb860dc3fdcf9be0d25ed49c5a
Shehzad Anwar <cougar59912@gmail.com>
2020-10-17 18:16:55 -0700

    Lab 2

---
+++ Lab2/Lab2.java
@@ -0,0 +1,48 @@
+//***********************************************
+//Name: Shehzad Anwar
+//Lab Letter: H
+//Title: Lab2.java
+//Description: Lab2
+//Date: 09/06/19
+//***********************************************
+public class Lab2 {
+
+	public static void main(String[] args) 
+	{
+		//TASK 1
+		char a='G';
+		int i = 89;
+		byte b = 4;
+		short s = 56;
+		double d = 4.355453532;
+		float f= 4.7333434f;
+		System.out.println("char: "+a);
+		System.out.println("integer: "+i);
+		System.out.println("byte: "+b);
+		System.out.println("short: "+s);
+		System.out.println("float: "+f);
+		System.out.println("double: "+d+"\n");
+		
+		//TASK 2
+		int A=1; int B=2;
+		
+		double num1 = A+B;
+		double num2 = A-B;
+		double num3 = A*B;
+		double num4 = A/B;
+		double num5 = A%B;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		
+		//TASK 3
+		System.out.println("\n"+A++);
+		System.out.println(++A);
+		System.out.println(A--);
+		System.out.println(--A);
+
+	}
+
+}
